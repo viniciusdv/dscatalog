162341b0e10823ed974402d84374c6464c093d9f
Vinicius Vivian <vinicius.vivian@catolicasc.org.br>
2021-01-20 20:58:44 -0300

    Post new Category

--- backend/src/main/java/com/dscatalog/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/dscatalog/dscatalog/resources/CategoryResource.java
@@ -1,17 +1,25 @@
 package com.dscatalog.dscatalog.resources;
 
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
+
+import javax.servlet.ServletSecurityElement;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 
 import com.dscatalog.dscatalog.dto.CategoryDTO;
-import com.dscatalog.dscatalog.entities.Category;
 import com.dscatalog.dscatalog.services.CategoryService;
+
 
 @RestController // Torna a classe em um controlador REST
 @RequestMapping(value =  "/categories") // Cria a rota de Acesso via API
@@ -37,6 +45,21 @@ public class CategoryResource {
 	 return ResponseEntity.ok().body(dto); // Retorna a lista em formato JSON , retornando a "DTO"
 	
      }
+      
+      @PostMapping // Insercao de registro
+      public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+    	  
+    	  dto = service.insert(dto);
+    	 
+    	 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+    			  .buildAndExpand(dto.getId()).toUri();
+    			    
+    			    
+    	  return ResponseEntity.created(uri).body(dto);
+    			  
+    	
+    	  
+      }
 	
 	
 
