dc2376ee4450100cf02f3288645bd779bba622d1
Vinicius Vivian <vinicius.vivian@catolicasc.org.br>
2021-01-24 15:02:35 -0300

    Product Entity

---
+++ backend/src/main/java/com/dscatalog/dscatalog/entities/Product.java
@@ -0,0 +1,150 @@
+package com.dscatalog.dscatalog.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.core.SerializableString;
+
+@Entity
+@Table(name = "tb_product")
+
+public class Product implements Serializable {
+	
+	
+
+	
+	private static final long serialVersionUID = 1L;
+	
+@Id // Define o ID no Banco
+@GeneratedValue(strategy = GenerationType.IDENTITY) // Gera o Auto_Increment no ID
+
+	private Long id;
+	private String name;
+	@Column(columnDefinition = "TEXT") // Faz com que o banco aceite o dado como TEXT , permitindo uma descrição mais longa
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant date;
+	
+	//Mapeamento JPA , Chave estrengeira entre produto e categoria
+	@ManyToMany
+	@JoinTable( name = "tb_product_category" ,
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	Set<Category> categories = new HashSet<>();
+	
+	public Product() {
+		
+		
+		
+	}
+
+	public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+	
+		
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+
+}
